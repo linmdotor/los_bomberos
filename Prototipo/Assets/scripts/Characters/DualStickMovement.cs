f244ad3ece4458c8619e3371555dcbe51b85b6eb
Bigtower25 <toni_torregrosa@msn.com>
2015-11-21 19:38:05 +0100

    Cosas + boton hacha

--- Prototipo/Assets/scripts/Characters/DualStickMovement.cs
+++ Prototipo/Assets/scripts/Characters/DualStickMovement.cs
@@ -5,28 +5,46 @@ using GamepadInput;
 
 public class DualStickMovement : MonoBehaviour
 {
-    static int nController = 1;
-    GamePad.Index mIndice;
+    private Rigidbody rb;
+    static int mIndice = 1;
+    GamePad.Index numController;
 
     public float speed = 5.0f;
 
     void Start()
-    {      
-        mIndice = (GamePad.Index)nController;
-        ++nController;
+    {
+        rb = GetComponent<Rigidbody>();
+        numController = (GamePad.Index)mIndice;
+        ++mIndice;
+    }
+
+    void FixedUpdate()
+    {
+        //Evitamos fuerzas residuales despues de chocar contra otro bombero
+        rb.velocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Vector3 leftStickAxis = (Vector3.right * GamePad.GetAxis(GamePad.Axis.LeftStick, mIndice).x + Vector3.forward * GamePad.GetAxis(GamePad.Axis.LeftStick, mIndice).y);
-        transform.position += leftStickAxis * speed * Time.deltaTime;
-        Vector3 lookDir = (Vector3.right * GamePad.GetAxis(GamePad.Axis.RightStick, mIndice).x + Vector3.forward * GamePad.GetAxis(GamePad.Axis.RightStick, mIndice).y);
+    {   
+        //Hacha
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SendMessage("useTool");
+        }
+
+        //Movemos al personaje
+        Vector3 leftStickAxis = (Vector3.right * GamePad.GetAxis(GamePad.Axis.LeftStick, numController).x + Vector3.forward * GamePad.GetAxis(GamePad.Axis.LeftStick, numController).y);
+        transform.position += leftStickAxis * speed * Time.deltaTime;
+
+        //Orientamos al personaje
+        Vector3 lookDir = (Vector3.right * GamePad.GetAxis(GamePad.Axis.RightStick, numController).x + Vector3.forward * GamePad.GetAxis(GamePad.Axis.RightStick, numController).y);
         if (lookDir.sqrMagnitude > 0.0f)
         {
             transform.rotation = Quaternion.LookRotation(lookDir, Vector3.up);
         }
         
     }
+
 }
