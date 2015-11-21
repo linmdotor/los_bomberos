8223050a310a4187521ff6e347784f84f64ebc1b
Daniel <dani.parra.90@gmail.com>
2015-11-21 18:09:19 +0100

    Accion romper puerta anadir tag Door

---
+++ Prototipo/Assets/scripts/Characters/Axe.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Damager))]
+[RequireComponent(typeof(BoxCollider))]
+public class Axe : MonoBehaviour {
+
+    private BoxCollider m_boxCollider;
+    private Damager m_damager;
+    private Animator m_animator;
+
+    private float currentTime;
+
+	// Use this for initialization
+	void Start () {
+        m_boxCollider = gameObject.GetComponent<BoxCollider>();
+        m_damager = gameObject.GetComponent<Damager>();
+        m_animator = gameObject.GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime -= Time.deltaTime;
+        /*if (m_boxCollider.enabled && currentTime < 0.0)
+        {
+            currentTime = 0.0f;
+            m_boxCollider.enabled = false;
+        }*/
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Door"))
+        {
+            other.gameObject.SendMessage("OnDamage", m_damager.m_damage);
+            m_boxCollider.enabled = false;
+        }
+    }
+    public void activeAxe(float seconds)
+    {
+        currentTime = seconds;
+        m_boxCollider.enabled = true;
+        m_animator.SetTrigger("Attack");
+    }
+}
---
+++ Prototipo/Assets/scripts/IA/Door.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Life))]
+[RequireComponent(typeof(BoxCollider))]
+public class Door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnDead()
+    {
+        gameObject.active = false;
+    }
+}
