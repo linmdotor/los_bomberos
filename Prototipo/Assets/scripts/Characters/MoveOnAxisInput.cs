c59f45e435f8d862ec4201e87a4c3b1e4978ed35
Bigtower25 <toni_torregrosa@msn.com>
2015-11-21 12:18:15 +0100

    Pruebas toni

---
+++ Prototipo/Assets/scripts/Characters/MoveOnAxisInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnAxisInput : MonoBehaviour {
+
+    public float speed = 2.0f;
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += (Vector3.right * Input.GetAxis(horizontalAxis) + Vector3.forward * Input.GetAxis(verticalAxis)) * Time.deltaTime;
+
+	}
+}
