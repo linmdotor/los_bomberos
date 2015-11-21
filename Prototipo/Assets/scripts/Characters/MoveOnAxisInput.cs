b2eb11d3c2b3af3fbd5f8b07719a3fe6f991c291
Bigtower25 <toni_torregrosa@msn.com>
2015-11-21 13:34:03 +0100

    Por fin se mueve!!

--- Prototipo/Assets/scripts/Characters/MoveOnAxisInput.cs
+++ Prototipo/Assets/scripts/Characters/MoveOnAxisInput.cs
@@ -6,13 +6,20 @@ using GamepadInput;
 public class MoveOnAxisInput : MonoBehaviour {
 
     public string nController = "One";
-   
+
+    GamePad.Index mIndice;
+
     public float speed = 2.0f;
+
+    void Start()
+    {
+        mIndice = GamePad.Index.One;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.position += (Vector3.right * GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One).x + Vector3.up * GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One).y) * Time.deltaTime;
+        transform.position += (Vector3.right * GamePad.GetAxis(GamePad.Axis.LeftStick, mIndice).x + Vector3.forward * GamePad.GetAxis(GamePad.Axis.LeftStick, mIndice).y) * speed * Time.deltaTime;
 
 	}
 }
