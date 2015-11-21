6540b71d45814de32bdfb54a9d52fc9cec1f1309
KaitoKid1111 <daleorei@gmail.com>
2015-11-21 16:47:56 +0100

    Creado Extinguisher; Creado prefab Extintor; Añadido Water FX Particles.

--- Prototipo/Assets/scripts/IA/GameManager.cs
+++ Prototipo/Assets/scripts/IA/GameManager.cs
@@ -5,8 +5,9 @@ public class GameManager : MonoBehaviour {
 
     public GameObject[] m_Players = null;
     public static uint m_NumPlayers = 4;
-    private bool[] m_DeadPlayers = new bool[4];
-
+    private bool[] m_DeadPlayers = new bool[m_NumPlayers];
+    private uint m_People = 10; //Esto debería ser rellenado por LevelManager.
+    private uint m_Cats = 3;    //Esto debería ser rellenado por LevelManager.
 
 	// Use this for initialization
 	void Start () {
@@ -18,9 +19,14 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Éstas comprobaciones son más óptimas como eventos en las correspondientes clases.
         if (m_DeadPlayers[1] && m_DeadPlayers[2] && m_DeadPlayers[4] && m_DeadPlayers[4])
         {
             Application.LoadLevel("GameOver");
+        }
+        if (m_People + m_Cats == 0)
+        {
+            Application.LoadLevel("Victory");
         }
 	}
 }
---
+++ Prototipo/Assets/scripts/Tools/Extinguisher.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Extinguisher : MonoBehaviour {
+
+    public Transform m_ShootPoint = null;
+    public GameObject m_AoE = null;
+    public GameObject m_Water = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Funciones llamadas desde el input para activar y desactivar el extintor. Tanto collider como efecto.
+    void startShooting()
+    {
+        m_AoE.GetComponent<Collider>().enabled = true;
+        m_Water.SetActive(true);
+    }
+    void stopShooting()
+    {
+        m_AoE.GetComponent<Collider>().enabled = false;
+        m_Water.SetActive(false);
+    }
+}
