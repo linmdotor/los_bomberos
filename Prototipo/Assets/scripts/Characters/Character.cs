02aa8610a178bd8a3403eda13723f4f519bf1da5
KaitoKid1111 <daleorei@gmail.com>
2015-11-21 21:43:27 +0100

    Alguna mierda.

--- Prototipo/Assets/scripts/Characters/Character.cs
+++ Prototipo/Assets/scripts/Characters/Character.cs
@@ -2,14 +2,32 @@
 using System.Collections;
 
 public class Character : MonoBehaviour {
+    private string m_SuitType = null;
+    private int m_DeathTime = 10;
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    //Las opciones de traje son 2: Ignifugo y Normal.
+    void setSuitType(string suit){
+        m_SuitType = suit;
+        if (m_SuitType == "Ignifugo")
+        {
+            m_DeathTime += 10;
+        }
+    }
+
+    void isDead()
+    {
+        if (m_DeathTime < 0)
+        {
+            this.SendMessage(this.name);
+        }
+    }
 }
---
+++ Prototipo/Assets/scripts/ClickedButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClickedButton : MonoBehaviour {
+    
+    public Dropdown m_Equipment;
+
+    private string[] m_EquipmentList = new string[3];
+	// Use this for initialization
+	void Start () {
+        m_EquipmentList[0] = "Axe";
+        m_EquipmentList[0] = "HugeEx";
+        m_EquipmentList[0] = "FireSuit";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlayerReady()
+    {
+        this.SendMessage("setReadyPlayer", this.name);
+        object[] options = new object[2];
+        options[0] = this.name;
+        options[1] = m_EquipmentList[m_Equipment.value];
+        this.SendMessage("setOptions", options);
+    }
+}
