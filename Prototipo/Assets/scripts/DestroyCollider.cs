c64c6d5209ff624178ce64f84deef986559b02f8
Lin M. Dotor <linmdotor@gmail.com>
2015-11-21 15:47:58 +0100

    Añadido el componente para los NPC, que desaparecen al tocarlos

---
+++ Prototipo/Assets/scripts/DestroyCollider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Este componente se pone a los objetos que queremos que desaparezcan al colisionar con el "Player" 
+ * Para que funcione, el GameObject debe tener un Collider, con el "isTrigger" activado.
+ */
+public class DestroyCollider : MonoBehaviour
+{
+    public string m_Tag = "Player";
+    //public float m_damage = 10.0f;
+    public bool m_destroyOnCollision = true;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag(m_Tag))
+        {
+            //other.gameObject.SendMessage("damage", m_damage, SendMessageOptions.DontRequireReceiver);
+            if (m_destroyOnCollision)
+                Destroy(gameObject);
+        }
+    }
+}
