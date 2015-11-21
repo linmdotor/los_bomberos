5d6c8fd69450e8bdbe21b5ba775a04b9e2f29bbe
KaitoKid1111 <daleorei@gmail.com>
2015-11-21 13:24:21 +0100

    Script States.

---
+++ Prototipo/Assets/scripts/Characters/States.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class States : MonoBehaviour {
+
+
+	/*// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}*/
+
+    public enum CharacterStates { NORMAL, BURNING, WET }
+
+    float changeState(CharacterStates state){
+        switch (state){
+            case CharacterStates.NORMAL:
+                return 100;
+            case CharacterStates.BURNING:
+                //Llamada a iniciar contador de muerte;
+                return 0;
+            case CharacterStates.WET:
+                return 50;
+            default:
+                return 100;
+        }
+    }
+}
