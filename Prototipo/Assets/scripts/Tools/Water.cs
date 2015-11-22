da44d444dc72b93d18fd2b22edd213f98589c3b9
Daniel <dani.parra.90@gmail.com>
2015-11-22 02:22:28 +0100

    activar estad agua

---
+++ Prototipo/Assets/scripts/Tools/Water.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            other.gameObject.SendMessage("OnWater");
+        }
+
+    }
+}
